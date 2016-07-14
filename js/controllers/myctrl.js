a6916a2f706509ca6c6c13cd5b262f9392e8c854
Raquel Lorenzana <raquellorenzana@raquels-air.attlocal.net>
2016-07-14 14:55:53 -0700

    first commit

---
+++ js/controllers/myctrl.js
@@ -0,0 +1,100 @@
+app.controller('topSfCtrl', function($scope) {
+	$scope.sf = [	
+		{topNum: "1.", restaurant: "Volta"}, 
+		{topNum: "2.", restaurant: "China Live"}, 
+		{topNum: "3.", restaurant: "The Manufactory"}, 
+		{topNum: "4.", restaurant: "Mr. Jiu's"}, 
+		{topNum: "5.", restaurant: "Citizen Fox"}, 
+		{topNum: "6.", restaurant: "Nightbird"}, 
+		{topNum: "7.", restaurant: "Brezza Emporio and Pizzeria"}, 
+		{topNum: "8.", restaurant: "Kraft & Hitachino"}, 
+		{topNum: "9.", restaurant: "Arsenal"}, 
+		{topNum: "10.", restaurant: "The Perennial"} 
+		]
+	})
+
+app.controller('topAustinCtrl', function($scope) {
+	$scope.austin = [	
+		{topNum: "1.", restaurant: "Veracruz All Natural"}, 
+		{topNum: "2.", restaurant: "East Side King Thai Kun"}, 
+		{topNum: "3.", restaurant: "General Tso'boy"}, 
+		{topNum: "4.", restaurant: "Jinya Ramen Bar"}, 
+		{topNum: "5.", restaurant: "Ola Poke"}, 
+		{topNum: "6.", restaurant: "Kyoten Sushiko"}, 
+		{topNum: "7.", restaurant: "Musashino Sushi Dokoro"}, 
+		{topNum: "8.", restaurant: "L'Oca D'Oro"}, 
+		{topNum: "9.", restaurant: "Yatai"}, 
+		{topNum: "10.", restaurant: "Little Sheep Mongolian Hot Pot"} 
+		]
+})
+
+app.controller('topNycCtrl', function($scope) {
+		$scope.nyc = [	
+		{topNum: "1.", restaurant: "Mission Chinese Food"}, 
+		{topNum: "2.", restaurant: "Russ & Daughters Cafe"}, 
+		{topNum: "3.", restaurant: "Wildair"}, 
+		{topNum: "4.", restaurant: "Uncle Boons"}, 
+		{topNum: "5.", restaurant: "Balthazar Restaurant"}, 
+		{topNum: "6.", restaurant: "The Dutch"}, 
+		{topNum: "7.", restaurant: "Lure Fishbar"}, 
+		{topNum: "8.", restaurant: "Katz's Delicatessen"}, 
+		{topNum: "9.", restaurant: "Estela"}, 
+		{topNum: "10.", restaurant: "Emilio's Ballato"} 
+		]
+})
+
+app.controller('topBostonCtrl', function($scope) {
+		$scope.boston = [	
+		{topNum: "1.", restaurant: "Alden & Harlow"}, 
+		{topNum: "2.", restaurant: "Area Four"}, 
+		{topNum: "3.", restaurant: "Asta"}, 
+		{topNum: "4.", restaurant: "B&G Oysters"}, 
+		{topNum: "5.", restaurant: "The Bancfroft"}, 
+		{topNum: "6.", restaurant: "Banyan Bar + Refuge"}, 
+		{topNum: "7.", restaurant: "Bergamot"}, 
+		{topNum: "8.", restaurant: "Bistro du Midi"}, 
+		{topNum: "9.", restaurant: "Bondir"}, 
+		{topNum: "10.", restaurant: "Brewers Fork"} 
+		]
+})
+
+app.controller('topMiamiCtrl', function($scope) {
+		$scope.miami = [	
+		{topNum: "1.", restaurant: "Alter"}, 
+		{topNum: "2.", restaurant: "27 Restaurant & Bar"}, 
+		{topNum: "3.", restaurant: "Klima"}, 
+		{topNum: "4.", restaurant: "Proof Pizza & Pasta"}, 
+		{topNum: "5.", restaurant: "Coya"}, 
+		{topNum: "6.", restaurant: "Strip Steak"}, 
+		{topNum: "7.", restaurant: "Cena by Michy"}, 
+		{topNum: "8.", restaurant: "Bazi"}, 
+		{topNum: "9.", restaurant: "Quality Meats"}, 
+		{topNum: "10.", restaurant: "Byblos"} 
+		]
+})
+
+app.controller('topLaCtrl', function($scope) {
+		$scope.la = [	
+		{topNum: "1.", restaurant: "Howlin Rays"}, 
+		{topNum: "2.", restaurant: "Moruno"}, 
+		{topNum: "3.", restaurant: "Kali"}, 
+		{topNum: "4.", restaurant: "Highland Park Bowl"}, 
+		{topNum: "5.", restaurant: "Salts Cure"}, 
+		{topNum: "6.", restaurant: "The Cannibal"}, 
+		{topNum: "7.", restaurant: "Norah"}, 
+		{topNum: "8.", restaurant: "Amboy"}, 
+		{topNum: "9.", restaurant: "Sawyer"}, 
+		{topNum: "10.", restaurant: "Wexlers Deli"} 
+		]
+	})
+
+
+
+
+
+
+
+
+
+
+
